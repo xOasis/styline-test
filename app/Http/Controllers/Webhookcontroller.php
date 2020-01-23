f6bc79ff3086e1424ca99f02493428dc9d1fcb8e
Rashik Zaman <rashikzaman13@gmail.com>
2020-01-23 15:51:43 +0600

    added notepad

--- app/Http/Controllers/Webhookcontroller.php
+++ app/Http/Controllers/Webhookcontroller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class WebhookController extends Controller
 {
@@ -12,8 +13,9 @@ class WebhookController extends Controller
 
     public function webhookPost(Request $request) {
         //return $request->all();
-        Log::info('message', $request->all());
+        //Log::info('message', $request->all());
 
+        Storage::put('attempt1.txt', $request->all());
         return $request->all();
     }
 }
